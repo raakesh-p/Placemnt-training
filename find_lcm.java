469637975a74f81de1860a9551ef71503a323fb0
Raakesh <raakesh362@gmail.com>
2026-09-08 21:00:46 +0530

    This time code to find lcm, well it was from a new prespective

---
+++ find_lcm.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class find_lcm{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter two numbers: ");
+    int a = s.nextInt();
+    int b = s.nextInt();
+    int c = Math.max(a,b);
+    int d = Math.min(a,b);
+    int step = c;
+    while(true){
+      if(c%d == 0)
+        break;
+      else
+        c = c + step;
+    }
+    System.out.println("LCM of " + a + " and " + b + " is " + c);
+  }
+ }
