41f632a52d5c8303ae56232d2f4ce574cd19cff5
Raakesh <raakesh362@gmail.com>
2026-09-08 20:30:02 +0530

    git ignore file was added along with some boring checking leap year code

---
+++ check_leap_year.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class check_leap_year{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter a year");
+    int year = s.nextInt();
+    if( year%400 == 0 || (year%4 == 0 && year%100 != 0))
+      System.out.println("The year " + year + " is a leap year");
+    else
+      System.out.println("The year " + year + " is not a leap year");
+  }
+}
