efc2597226c8c53f900894910859676ebaa1759a
Raakesh <raakesh362@gmail.com>
2026-09-12 07:45:21 +0530

    A alternative method for monkey_and_tower problem

---
+++ enhanced_monkey_and_tower.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class enhanced_monkey_and_tower{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    
+    System.out.print("Enter the height of the tower: ");
+    int towerHeight = s.nextInt();
+    System.out.print("Enter the climbing distance: ");
+    int ascend = s.nextInt();
+    System.out.print("Enter the slipping diatance: ");
+    int descend = s.nextInt();
+    
+    int net = ascend - descend;
+    
+    int preFinal = towerHeight - ascend;
+    
+    int pft = preFinal / net;
+    
+    System.out.println(2*pft + 1);
+  }
+}
+
